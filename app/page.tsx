03b8b14bb0818a69192f3c456e905395d75dab24
expeditus303 <ricardotonietto@gmail.com>
2023-11-11 14:27:03 -0300

    feat: percentage bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 export default function Page() {
-
   const MOCKDATA = {
     base: {
       currency_code: "USD",
@@ -7,37 +6,37 @@ export default function Page() {
       current_date: "2023-10-31",
       ath: {
         date: "2021-11-10",
-        price: 69000
+        price: 69000,
       },
-      current_price: 34000
+      current_price: 34000,
     },
     rates: {
       BRL: {
         currency_name: "Brazilian Real",
         ath: {
           exchange_rate: 5.4477,
-          price: 375891.3
+          price: 375891.3,
         },
         current: {
           exchange_rate: 5.2154,
-          price: 177323.6
+          price: 177323.6,
         },
-        variation_since_ath: -52.82
+        variation_since_ath: -52.82,
       },
       EUR: {
         currency_name: "Euro",
         ath: {
           exchange_rate: 0.85,
-          price: 58650
+          price: 58650,
         },
         current: {
           exchange_rate: 0.88,
-          price: 29920
+          price: 29920,
         },
-        variation_since_ath: -49
-      }
-    }
-  }
+        variation_since_ath: -49,
+      },
+    },
+  };
 
   return (
     <main className="flex min-h-screen flex-col p-4 bg-orange-200">
@@ -73,7 +72,23 @@ export default function Page() {
               <div className="font-bold">BRL</div>
               <div className="text-xs italic">Currency</div>
             </div>
-            <div className="w-1/2 bg-red-500 shrink-0">load bar</div>
+            <div className="flex items-center w-1/2 shrink-0">
+              {/* <div className="flex justify-between mb-1">
+                <span className="text-base font-medium text-blue-700 dark:text-white">
+                  Flowbite
+                </span>
+                <span className="text-sm font-medium text-blue-700 dark:text-white">
+                  45%
+                </span>
+              </div> */}
+              <div className="w-full bg-gray-100 rounded-md h-7">
+                <div
+                  className="flex items-center bg-green-400 h-7 rounded-md pl-2 box-border text-white font-semibold"
+                  style={{ width: "45%" }}
+                >-45%</div>
+                400
+              </div>
+            </div>
           </div>
 
           <div className="mt-4 flex justify-between">
@@ -85,7 +100,6 @@ export default function Page() {
             </div>
             <div className="w-1/2 bg-red-500 shrink-0">load bar</div>
           </div>
-
         </div>
       </div>
     </main>
